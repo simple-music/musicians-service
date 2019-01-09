9e415cabbfd14cc51edd2c43d53cbfc41f62cf16
denis96z <zinoviev.denis@yandex.ru>
2019-01-09 21:30:45 +0300

    controller implemented

--- src/main/kotlin/ru/bmstu/iu7/simplemusic/musiciansservice/controller/MusicianController.kt
+++ src/main/kotlin/ru/bmstu/iu7/simplemusic/musiciansservice/controller/MusicianController.kt
@@ -1,8 +1,10 @@
 package ru.bmstu.iu7.simplemusic.musiciansservice.controller
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder
 import ru.bmstu.iu7.simplemusic.musiciansservice.domain.Musician
 import ru.bmstu.iu7.simplemusic.musiciansservice.model.MusicianUpdate
 import ru.bmstu.iu7.simplemusic.musiciansservice.model.NewMusician
@@ -13,22 +15,33 @@ class MusicianController(@Autowired private val musicianService: MusicianService
 
     @PostMapping
     fun addMusician(@RequestBody newMusician: NewMusician): ResponseEntity<Musician> {
-        TODO("not implemented")
+        val musician = this.musicianService.addMusician(newMusician)
+        val uri = MvcUriComponentsBuilder
+                .fromController(this::class.java).path("/{id}")
+                .buildAndExpand(musician.id).toUri()
+        return ResponseEntity.created(uri).body(musician)
     }
 
     @GetMapping("/{id}")
     fun getMusician(@PathVariable("id") musicianId: String): ResponseEntity<Musician> {
-        TODO("not implemented")
+        val musician = this.musicianService.getMusician(musicianId)
+        return ResponseEntity.ok(musician)
     }
 
     @PatchMapping("/{id}")
     fun updateMusician(@PathVariable("id") musicianId: String,
-                       @RequestBody musicianUpdate: MusicianUpdate): ResponseEntity<Musician?> {
-        TODO("not implemented")
+                       @RequestBody musicianUpdate: MusicianUpdate): ResponseEntity<Musician> {
+        val musician = this.musicianService.updateMusician(musicianId, musicianUpdate)
+        return if (musician == null) {
+            ResponseEntity.status(HttpStatus.NOT_MODIFIED.value()).build()
+        } else {
+            ResponseEntity.ok(musician)
+        }
     }
 
     @DeleteMapping("/{id}")
-    fun deleteMusician(@PathVariable("id") musicianId: String): ResponseEntity<Musician?> {
-        TODO("not implemented")
+    fun deleteMusician(@PathVariable("id") musicianId: String): ResponseEntity<Musician> {
+        this.musicianService.deleteMusician(musicianId)
+        return ResponseEntity.noContent().build()
     }
 }
